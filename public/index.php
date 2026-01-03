dea2bbf2f8d3adadb764b76856d45bc0dd735744
snova <ssnovaa@gmail.com>
2026-01-03 15:37:21 +0200

    Enable debug errors in index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+// 🔥 ДОБАВИТЬ ЭТИ СТРОКИ ДЛЯ ОТЛАДКИ БЕЛОГО ЭКРАНА
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
